b7966194050e7879fb26fd47315375e87170f942
Alexander Suesskind <s87424@bht-berlin.de>
2025-07-19 21:33:56 +0200

    Fix script logic

--- styles/scripts/script.js
+++ styles/scripts/script.js
@@ -3,34 +3,22 @@ const tiles = document.querySelectorAll('.tile');
 const numberTiles = tiles.length;
 const cycleDuration = TILE_DURATION * numberTiles;
 
-// tiles.forEach((tile, index) => {
-//   const run = () => {
-//     const initialDelay = index * TILE_DURATION;
-//     setTimeout(function loop() {
-//       tile.style.animationPlayState = 'running';
-//       setTimeout(() => {
-//         tile.style.animationPlayState = 'paused';
-//       }, TILE_DURATION);
-//       setTimeout(loop, cycleDuration);
-//     }, initialDelay);
-//   };
-//   run();
-// });
-
 tiles.forEach((tile, index) => {
-  const run = () => {
-    const initialDelay = index * TILE_DURATION;
-    setTimeout(function loop() {
-      tile.style.animation = 'none'; // Reset animation
-      tile.style.transform = 'none'; // Explicitly reset transform
-      tile.offsetHeight; // Force reflow
-      tile.style.animation = ''; // Re-apply animation from CSS
-      tile.style.animationPlayState = 'running';
-      setTimeout(() => {
-        tile.style.animationPlayState = 'paused';
-      }, TILE_DURATION);
-      setTimeout(loop, cycleDuration);
-    }, initialDelay);
-  };
-  run();
+  const initialDelay = index * TILE_DURATION;
+
+  function animateTile() {
+    tile.style.animation = 'none';
+    tile.style.transform = 'none';
+    tile.offsetHeight;
+    tile.style.animation = '';
+    tile.style.animationPlayState = 'running';
+    setTimeout(() => {
+      tile.style.animationPlayState = 'paused';
+    }, TILE_DURATION);
+    setTimeout(animateTile, cycleDuration);
+  }
+
+  setTimeout(() => {
+    animateTile();
+  }, initialDelay);
 });
